7125f57313bcfce3cc0674f7346882facd431c63
Usuario <juan.rodriguez215@alu.ulpgc.es>
2025-03-17 20:15:48 +0000

    Functional commit.

--- Scripts/main/player_1.gd
+++ Scripts/main/player_1.gd
@@ -1,19 +1,12 @@
 extends CharacterBody2D
 
-const SPEED = 200
+const SPEED = 1000
 const JUMP_VELOCITY = -400
 const GRAVITY = 500
 
 @onready var sprite = $AnimatedSprite2D
 
-var fall_limit = 750
-
 func _physics_process(delta):
-	# Solo mover si este jugador es el activo en Level1
-	if get_parent().active_player != self:
-		velocity.x = 0
-		sprite.play("idle")
-		return
 
 	# Aplicar gravedad
 	if not is_on_floor():
@@ -33,9 +26,6 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	if position.y > fall_limit:
-		respawn()
-
 	# Cambio de animaciones
 	if not is_on_floor():
 		sprite.play("jump")
@@ -45,7 +35,3 @@ func _physics_process(delta):
 		sprite.play("idle")
 
 	move_and_slide()
-
-func respawn():
-	position = Vector2(0, 200)
-	velocity = Vector2.ZERO
