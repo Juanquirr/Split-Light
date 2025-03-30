0ddf1c83c5cfd5d44fc25a7c48840eab3237cf66
IanSamuelTrujilloGil <iansamueltrujillo@@gmail.com>
2025-03-30 21:39:49 +0100

    Fixed interaction bounds in Door component to prevent unintended access.

--- Scripts/Components/door/door.gd
+++ Scripts/Components/door/door.gd
@@ -1,12 +1,26 @@
-extends Sprite2D
+extends Area2D
 
 var key: Node  
 var target_scene: SceneManager.SCENES
+var players_inside = []
 
 func init(target_scene: SceneManager.SCENES, key = null ) -> void:
 	self.key = key  
 	self.target_scene = target_scene 
 	
 func _process(_delta):
-	if Input.is_action_just_pressed("Interact") && (key == null || key.get_key_status()):
-		SceneManager.change_to_scene(self.target_scene)
+	if Input.is_action_just_pressed("Interact"):
+		for player in players_inside:
+				if player.is_active_player() && (key == null || key.get_key_status()):
+					SceneManager.change_to_scene(self.target_scene)
+		
+		
+func _on_body_entered(body):
+	if body.name.begins_with("Player"):
+		players_inside.append(body)
+		
+
+func _on_body_exited(body):
+	if body in players_inside:
+		players_inside.erase(body)
+		
