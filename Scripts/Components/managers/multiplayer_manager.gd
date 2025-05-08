d1b4662458d52bbd9965ef323604b29bb1a9e77f
SkinnyDevi <skinnydevi.dev@gmail.com>
2025-05-08 14:04:53 +0100

    Synchronized menu quit and finish to menu actions

--- Scripts/Components/managers/multiplayer_manager.gd
+++ Scripts/Components/managers/multiplayer_manager.gd
@@ -4,30 +4,36 @@ var IS_MULTIPLAYER := false
 var IS_HOST := false
 var IS_CLIENT := false
 var HOST := "localhost"
-var PORT := -1
+const PORT := 49593 # Fixed port
 var CLIENT_ID := -1
 
 var SERVER: ENetMultiplayerPeer = ENetMultiplayerPeer.new()
 
+func _ready() -> void:
+	multiplayer.peer_connected.connect(_on_peer_connect)
+	multiplayer.peer_disconnected.connect(_on_peer_disconnect)
+
 func debug_print_state():
 	print("IS MULTIPLAYER: %s" % IS_MULTIPLAYER)
 	print("IS HOST: %s" % IS_HOST)
+	print("SELF ID: %s" % multiplayer.get_unique_id())
 	print("IS CLIENT: %s" % IS_CLIENT)
-	print("PORT: %s" % PORT)
 	print("CLIENT ID: %s" % CLIENT_ID)
+	print("PORT: %s" % PORT)
 
 func _reset_multiplayer_state():
 	self.IS_MULTIPLAYER = false
 	self.IS_HOST = false
 	self.IS_CLIENT = false
-	self.PORT = -1
 	self.CLIENT_ID = -1
 
 func _on_peer_connect(id: int):
+	if not MultiplayerManager.IS_HOST: return
 	print_debug("Peer connected: %s" % id)
 	self.CLIENT_ID = id
 
 func _on_peer_disconnect(id: int):
+	if not MultiplayerManager.IS_HOST: return
 	print_debug("Peer disconnected: %s" % id)
 	self.CLIENT_ID = -1
 
@@ -37,36 +43,40 @@ func close_multiplayer():
 	self._reset_multiplayer_state()
 	print_debug("Server closed.")
 
-func connect_server_signals():
-	multiplayer.peer_connected.connect(_on_peer_connect)
-	multiplayer.peer_disconnected.connect(_on_peer_disconnect)
-
 func create_server() -> int:
-	var port = randi_range(49152, 65535)
-	var error := SERVER.create_server(port, 1)
+	var error := self.SERVER.create_server(self.PORT, 1)
 	if error != OK:
 		push_error("Could not create server: %s" % error)
+		self._reset_multiplayer_state()
 		return -1
 	
-	self.PORT = port
 	self.IS_MULTIPLAYER = true
 	self.IS_HOST = true
-	multiplayer.multiplayer_peer = SERVER
-	self.connect_server_signals()
+	multiplayer.multiplayer_peer = self.SERVER
 	print_debug("Created server on port %s" % self.PORT)
 	return self.PORT
 
-func join_server(port: int) -> bool:
-	var error := self.SERVER.create_client(self.HOST, port)
+func join_server(_room_code: int) -> bool:
+	var error := self.SERVER.create_client(self.HOST, self.PORT)
 	if error != OK:
 		push_error("Could not join server: %s" % error)
 		self._reset_multiplayer_state()
 		return false
 	
-	self.PORT = port
 	self.IS_MULTIPLAYER = true
 	self.IS_CLIENT = true
-	multiplayer.multiplayer_peer = SERVER
+	multiplayer.multiplayer_peer = self.SERVER
 	self.CLIENT_ID = multiplayer.get_unique_id()
-	print_debug("Connected to server on port %s" % PORT)
+	print_debug("Connected to server on port %s" % self.PORT)
+	return true
+	
+func disconnect_from_server() -> bool:
+	if not IS_CLIENT:
+		print_debug("Not a client, cannot disconnect.")
+		return false
+	
+	self.SERVER.close()
+	multiplayer.multiplayer_peer = null
+	self._reset_multiplayer_state()
+	print_debug("Disconnected from server.")
 	return true
--- Scripts/exit.gd
+++ Scripts/exit.gd
@@ -8,8 +8,26 @@ func _on_pressed() -> void:
 
 func _on_cancel_quit_pressed() -> void:
 	ConfirmQuitScreen.hide()
+	
+func wait(seconds: float) -> void:
+	await get_tree().create_timer(seconds).timeout
+	
+@rpc("any_peer", "call_remote")
+func _force_server_quit():
+	if MultiplayerManager.IS_CLIENT:
+		MultiplayerManager.disconnect_from_server()
+	SceneManager.change_to_scene(SceneManager.GameScenes.MAIN_MENU)
 
 func _on_confirm_quit_pressed() -> void:
 	PauseMenu.unpause()
-	SceneManager.change_to_scene(SceneManager.GameScenes.MAIN_MENU)	
+	
+	if MultiplayerManager.IS_MULTIPLAYER:
+		if MultiplayerManager.IS_CLIENT:
+			MultiplayerManager.disconnect_from_server()
+		elif MultiplayerManager.IS_HOST:
+			_force_server_quit.rpc_id(MultiplayerManager.CLIENT_ID)
+			await wait(1)
+			MultiplayerManager.close_multiplayer()
+	
+	SceneManager.change_to_scene(SceneManager.GameScenes.MAIN_MENU)
 	
