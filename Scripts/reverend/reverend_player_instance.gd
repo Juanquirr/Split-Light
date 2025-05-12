86def27264418a032e68eddfd6941c884acfc6b7
SkinnyDevi <skinnydevi.dev@gmail.com>
2025-05-12 19:30:48 +0100

    Added modular methods for adding sounds to players

--- Scripts/reverend/reverend_player_instance.gd
+++ Scripts/reverend/reverend_player_instance.gd
@@ -9,12 +9,15 @@ func _ready() -> void:
 	
 	configure_multiplayer()
 	configure_as_client()
+	
+func sound_process() -> void:
+	if direction != 0 and is_on_floor():
+		AudioManagerInstance.create_variant_audio(VariantSoundEffect.VARIANT_SOUND_EFFECT_TYPE.ON_STONE_WALK)
 
 func animation_process():
 	if not is_on_floor():
 		self.animated_sprite.play("reverend_jump")
 	elif direction != 0 and is_on_floor():
 		self.animated_sprite.play("reverend_walk")
-		AudioManagerInstance.create_variant_audio(VariantSoundEffect.VARIANT_SOUND_EFFECT_TYPE.ON_STONE_WALK)
 	else:
 		self.animated_sprite.play("reverend_idle")
--- lib/AudioManager/SoundEffectSetting.gd
+++ lib/AudioManager/SoundEffectSetting.gd
@@ -5,7 +5,7 @@ class_name SoundEffect
 
 ## Stores the different types of sounds effects available to be played to distinguish them from another. Each new SoundEffect resource created should add to this enum, to allow them to be easily instantiated via [method AudioManager.create_2d_audio_at_location()] and [method AudioManager.create_audio()].
 enum SOUND_EFFECT_TYPE {
-	ON_GUN_FIRE
+	DEFAULT_SOUND
 }
 
 @export_range(0, 10) var limit: int = 5 ## Maximum number of this SoundEffect to play simultaneously before culled.
