dcc9c9856a23ab456a4b20ccde5615f07cab9c5b
SkinnyDevi <skinnydevi.dev@gmail.com>
2025-05-13 17:32:05 +0100

    Added drop sound key on drop event

--- Scripts/level_1/key.gd
+++ Scripts/level_1/key.gd
@@ -2,11 +2,15 @@ extends InteractiveKey
 
 
 class BaseInventoryKey extends BaseInventoryItem:
-	var key_on_floor = false
-
-	func wait(seconds: float) -> void:
-		await self.parent_node.get_tree().create_timer(seconds).timeout
-
+	func play_drop_sound():
+		AudioManagerInstance.create_audio(
+			SoundEffect.SOUND_EFFECT_TYPE.ON_KEY_DROP
+		)
+	
+	func _init(icon: Sprite2D, init_parent_node: Node2D) -> void:
+		super._init(icon, init_parent_node)
+		self.touched_floor.connect(play_drop_sound)
+	
 	func take():
 		super.take()
 		self.parent_node.is_taken = true
@@ -20,12 +24,6 @@ class BaseInventoryKey extends BaseInventoryItem:
 		self.parent_node.is_taken = false
 		self.parent_node.make_invisible()
 		self.parent_node.make_visible()
-		
-	func _process(delta: float) -> void:
-		super._process(delta)
-		# TODO: add logic to check when a collision with
-		# floor happens,
-		# play key_drop audio
 
 func _ready() -> void:
 	var inventory_key: BaseInventoryItem = BaseInventoryKey.new($Sprite2D, self)
