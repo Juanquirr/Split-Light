82a38f3f1b6bc3e577a2d815301c9c82d78b3fe1
Juanquirr <juanquirr579@gmail.com>
2025-05-15 18:32:19 +0100

    Major refactor in LVL 1 to follow clean code standars.

--- Scripts/level_1/level_1.gd
+++ Scripts/level_1/level_1.gd
@@ -1,22 +1,23 @@
 extends Node2D
 
-@onready var player1: PlayerInstance = $Player1
-@onready var player2: PlayerInstance = $Player2
-@onready var player_switch_manager = $PlayerSwitchManager
-@onready var label = $Count
+@onready var player1: PlayerInstance = $Characters/Player1
+@onready var player2: PlayerInstance = $Characters/Player2
+@onready var player_switch_manager = $Characters/PlayerSwitchManager
+@onready var label = $Mechanisms/Count
+@onready var key = $Mechanisms/key
 
 func _on_teleported_left(body):
-	if body == $Player1:
-		$key.disable_level_visibility()
-		$key.make_invisible()
-		$Count.sub_count()
+	if body == player1:
+		key.disable_level_visibility()
+		key.make_invisible()
+		label.sub_count()
 	
 func _on_teleported_right(body):
-	if body == $Player1:
-		$key.disable_level_visibility()
-		$key.make_invisible()
-		$Count.add_count()
+	if body == player1:
+		key.disable_level_visibility()
+		key.make_invisible()
+		label.add_count()
 
 func _count_number_reached():
-	$key.enable_level_visibility()
-	$key.make_visible()
+	key.enable_level_visibility()
+	key.make_visible()
--- Scripts/level_1/player_switch_manager.gd
+++ Scripts/level_1/player_switch_manager.gd
@@ -8,11 +8,11 @@ func _ready() -> void:
 	player2 = $"../Player2"
 	
 	player1_perspective_manager = [
-		$"../Count".visibility, 
-		$"../Area2D".visibility_black_poem, 
-		$"../key".interaction_manager
+		$"../../Mechanisms/Count".visibility, 
+		$"../../Mechanisms/Area2D".visibility_black_poem, 
+		$"../../Mechanisms/key".interaction_manager
 	]
-	player2_perspective_manager = [$"../Area2D".visibility_red_poem]
+	player2_perspective_manager = [$"../../Mechanisms/Area2D".visibility_red_poem]
 	
 	if MultiplayerManager.IS_MULTIPLAYER and MultiplayerManager.IS_CLIENT:
 		set_active_player(player2)
