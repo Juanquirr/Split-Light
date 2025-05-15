88e814110d0d5eb0b70bf2ce369893a6df245c8b
SkinnyDevi <skinnydevi.dev@gmail.com>
2025-05-15 13:02:41 +0100

    Added first key appearance sound

--- lib/AudioManager/SoundEffectSetting.gd
+++ lib/AudioManager/SoundEffectSetting.gd
@@ -11,6 +11,7 @@ enum SOUND_EFFECT_TYPE {
 	ON_BOOK_OPEN,
 	ON_BOOK_CLOSE,
 	ON_ABOT_JUMP,
+	ON_TASK_COMPLETE_1
 }
 
 @export_range(0, 10) var limit: int = 5 ## Maximum number of this SoundEffect to play simultaneously before culled.
